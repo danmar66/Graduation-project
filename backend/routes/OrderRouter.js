6ee67f5cb01a4f0e4f2797fac60edaf929ef01c9
Daniel Marchenko <marchenkodanil97@gmail.com>
2022-02-10 19:45:44 +0200

    Add middleware

--- backend/routes/OrderRouter.js
+++ backend/routes/OrderRouter.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const OrderController = require("../controllers/OrderController");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 const orderRouter = new express.Router();
 
-orderRouter.post("/order", OrderController.create);
-orderRouter.get("/order", OrderController.getAll);
+orderRouter.post("/order", authMiddleware, OrderController.create);
+orderRouter.put("/order", authMiddleware,OrderController.update);
+orderRouter.delete("/order/:id", authMiddleware, OrderController.delete);
 orderRouter.get("/order/:id", OrderController.getOne);
-orderRouter.put("/order", OrderController.update);
-orderRouter.delete("/order/:id", OrderController.delete);
+orderRouter.get("/order", OrderController.getAll);
 
 module.exports = orderRouter;
