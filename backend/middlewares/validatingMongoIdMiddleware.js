9e44bcbfb2ad8937e2fb431373e7cda0cb9539f7
Daniel Marchenko <marchenkodanil97@gmail.com>
2022-02-10 19:44:15 +0200

    Add validating mongodb id middleware

---
+++ backend/middlewares/validatingMongoIdMiddleware.js
@@ -0,0 +1,18 @@
+const ObjectID = require('mongoose').isValidObjectId
+
+module.exports = function (req, res, next) {
+
+    try {
+        const { id } = req.params
+        const isValid = ObjectID(id)
+
+        if (!isValid) {
+            return res.status(400).json({message: "Invalid ID!"})
+        }
+
+        next()
+    } catch (e) {
+        console.log(e);
+        return res.status(401).json({ message: "Validating error" });
+    }
+};
