02ae7e616b14779c32ec592d3cdef892829f4f59
Daniel Marchenko <marchenkodanil97@gmail.com>
2022-02-10 18:15:17 +0200

    Add validation for admin

--- backend/controllers/AdminController.js
+++ backend/controllers/AdminController.js
@@ -1,125 +1,135 @@
 const helper = require("../helpers/Helpers");
 const Admin = require("../models/Admin");
-const { validationResult } = require("express-validator");
+const {validationResult} = require("express-validator");
 
 class AdminController {
-  async create(req, res) {
-    try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ message: "Registration error", errors });
-      }
-      const { username, email, password } = req.body;
-      const candidate = await Admin.findOne({ $or: [{ username }, { email }] });
-      if (candidate) {
-        return res
-          .status(400)
-          .json({ message: "An administrator with the same data already exists" });
-      }
-      const passwordHash = helper.passwordHash(password);
-      await Admin.create({ username, email, password: passwordHash });
-      res.json({ message: `Admin ${username} added successfully` });
-    } catch (e) {
-      console.log(e.message);
-      res.status(424).json({ error: "Unknown error" });
-    }
-  }
+    async create(req, res) {
+        try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                const errorMessage = errors.errors.map(({param, msg}) => ({[param]: msg}))
+                return res.status(400).json({message: "Validation error", errorMessage});
+            }
 
-  async update(req, res) {
-    try {
-      const { _id, username, email, password } = req.body;
-      if (!_id) {
-        res.status(400).json({ message: `ID ${_id} not found` });
-      }
-      await Admin.findByIdAndUpdate(
-        _id,
-        { username, email, password: helper.passwordHash(password) },
-        { new: true }
-      );
-      return res.json({ message: `Admin with ID ${_id} edited` });
-    } catch (e) {
-      console.log(e.message);
-      res.status(424).json({ error: "Unknown error" });
-    }
-  }
+            const {username, email, password} = req.body;
+            const candidate = await Admin.findOne({$or: [{username}, {email}]});
+            if (candidate) {
+                return res
+                    .status(400)
+                    .json({message: "An administrator with the same data already exists"});
+            }
+            const passwordHash = helper.passwordHash(password);
+            const newAdmin = await Admin.create({username, email, password: passwordHash});
 
-  async delete(req, res) {
-    try {
-      const { id } = req.params;
-      if (!id) {
-        res.status(400).json({ message: "ID not found" });
-      }
-      if (id.match(/^[0-9a-fA-F]{24}$/)) {
-        const isDeleted = await Admin.findByIdAndDelete(id);
-        console.log(isDeleted);
-        if (isDeleted === null) {
-          res.status(400).json({ message: "ID not found" });
-        } else {
-          res.json({ message: `Admin with ID ${id} deleted` });
+            res.json({message: `Admin added`, newAdmin});
+        } catch (e) {
+            console.log(e.message);
+            res.status(424).json({error: "Unknown error"});
         }
-      }
-      res.json({ message: `Enter valid ID` });
-    } catch (e) {
-      console.log(e.message);
-      res.status(424).json({ error: "Unknown error" });
     }
-  }
 
-  async getAll(req, res) {
-    try {
-      const admins = await Admin.find();
-      return res.json(admins);
-    } catch (e) {
-      console.log(e.message);
-      res.status(424).json({ error: "Unknown error" });
-    }
-  }
-
-  async getOne(req, res) {
-    try {
-      const { id } = req.params;
-      if (id.match(/^[0-9a-fA-F]{24}$/)) {
-        if (!id) {
-          res.status(400).json({ message: "ID not found" });
+    async update(req, res) {
+        try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                const errorMessage = errors.errors.map(({param, msg}) => ({[param]: msg}))
+                return res.status(400).json({message: "Validation error", errorMessage});
+            }
+            const {id} = req.params;
+            console.log(id)
+            const {username, email, password} = req.body;
+            if (!id) {
+                res.status(400).json({message: `ID ${id} not found`});
+            }
+            const editedAdmin = await Admin.findByIdAndUpdate(
+                id,
+                {username, email, password: helper.passwordHash(password)},
+                {new: true}
+            );
+            return res.json({message: `Admin edited`, editedAdmin});
+        } catch (e) {
+            console.log(e.message);
+            res.status(424).json({error: "Unknown error"});
         }
-        const admin = await Admin.findById(id);
-        return res.json(admin);
-      }
-      res.json({ message: `Enter valid ID` });
-    } catch (e) {
-      console.log(e.message);
-      res.status(424).json({ error: "Unknown error" });
     }
-  }
 
-  async getUser(req, res) {
-    try {
-      const admin = await Admin.findById(req.user.id);
-      return res.json({ Username: admin.username, Email: admin.email });
-    } catch (e) {
-      console.log(e.message);
-      res.status(424).json({ error: "Unknown error" });
+    async delete(req, res) {
+        try {
+            const {id} = req.params;
+            if (!id) {
+                res.status(400).json({message: "ID not found"});
+            }
+            if (id.match(/^[0-9a-fA-F]{24}$/)) {
+                const deletedAdmin = await Admin.findByIdAndDelete(id);
+                if (deletedAdmin === null) {
+                    res.status(400).json({message: "ID not found"});
+                } else {
+                    res.json({message: `Admin deleted`, deletedAdmin});
+                }
+            }
+            res.json({message: `Enter valid ID`});
+        } catch (e) {
+            console.log(e.message);
+            res.status(424).json({error: "Unknown error"});
+        }
     }
-  }
 
-  async login(req, res) {
-    try {
-      const { username, password } = req.body;
-      const admin = await Admin.findOne({ username });
-      if (!admin) {
-        return res.status(400).json({ message: `Admin ${username} not found` });
-      }
-      const passwordCheck = helper.passwordCheck(password, admin.password);
-      if (!passwordCheck) {
-        return res.status(400).json({ message: `Error. Type the correct password` });
-      }
-      const token = helper.generateAccessToken(admin._id);
-      res.json({ token });
-    } catch (e) {
-      res.status(400).json({ message: "Login error" });
-      console.log(e.message);
+    async getAll(req, res) {
+        try {
+            const admins = await Admin.find();
+            return res.json(admins);
+        } catch (e) {
+            console.log(e.message);
+            res.status(424).json({error: "Unknown error"});
+        }
     }
-  }
+
+    async getOne(req, res) {
+        try {
+            const {id} = req.params;
+            if (id.match(/^[0-9a-fA-F]{24}$/)) {
+                if (!id) {
+                    res.status(400).json({message: "ID not found"});
+                }
+                const admin = await Admin.findById(id);
+                return res.json(admin);
+            }
+            res.json({message: `Enter valid ID`});
+        } catch (e) {
+            console.log(e.message);
+            res.status(424).json({error: "Unknown error"});
+        }
+    }
+
+    async getUser(req, res) {
+        try {
+            const admin = await Admin.findById(req.user.id);
+            return res.json({Username: admin.username, Email: admin.email});
+        } catch (e) {
+            console.log(e.message);
+            res.status(424).json({error: "Unknown error"});
+        }
+    }
+
+    async login(req, res) {
+        try {
+            const {username, password} = req.body;
+            const admin = await Admin.findOne({username});
+            if (!admin) {
+                return res.status(400).json({message: `Admin ${username} not found`});
+            }
+            const passwordCheck = helper.passwordCheck(password, admin.password);
+            if (!passwordCheck) {
+                return res.status(400).json({message: `Error. Type the correct password`});
+            }
+            const token = helper.generateAccessToken(admin._id);
+
+            res.json({message: "Login successful", token});
+        } catch (e) {
+            res.status(400).json({message: "Login error"});
+            console.log(e.message);
+        }
+    }
 }
 
 module.exports = new AdminController();
--- backend/routes/AdminRouter.js
+++ backend/routes/AdminRouter.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const AdminController = require("../controllers/AdminController");
 const authMiddleware = require("../middlewares/authMiddleware");
+const {check} = require('express-validator')
 
 const adminRouter = new express.Router();
 
-adminRouter.post("/admin/create", AdminController.create);
-adminRouter.put("/admin/edit/:id", AdminController.update);
-adminRouter.delete("/admin/delete/:id", AdminController.delete);
+adminRouter.post("/admin/create", [
+    check('username', 'Username must be filled').notEmpty(),
+    check('email', 'Email must be filled').isEmail().notEmpty(),
+    check('password', 'Password must be 4 to 8 characters').notEmpty().isLength({min: 4, max: 8}),
+], authMiddleware, AdminController.create);
+adminRouter.put("/admin/edit/:id", [
+    check('username', 'Username must be filled').notEmpty(),
+    check('email', 'Email must be filled').isEmail().notEmpty(),
+    check('password', 'Password must be 4 to 8 characters').notEmpty().isLength({min: 4, max: 8}),
+], authMiddleware, AdminController.update);
+adminRouter.delete("/admin/delete/:id", authMiddleware, AdminController.delete);
 adminRouter.get("/admin", authMiddleware, AdminController.getAll);
 adminRouter.get("/admin/user", authMiddleware, AdminController.getUser);
-adminRouter.get("/admin/:id", AdminController.getOne);
+adminRouter.get("/admin/:id", authMiddleware, AdminController.getOne);
 adminRouter.post("/admin/login", AdminController.login);
 
 module.exports = adminRouter;
