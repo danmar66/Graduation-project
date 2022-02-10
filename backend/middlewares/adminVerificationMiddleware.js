ca35a45563da3569302fc47b1d69af4510bc5f1e
Daniel Marchenko <marchenkodanil97@gmail.com>
2022-02-10 18:16:16 +0200

    Refactoring admin middleware

--- backend/middlewares/adminVerificationMiddleware.js
+++ backend/middlewares/adminVerificationMiddleware.js
@@ -1,21 +1,27 @@
 const Admin = require("../models/Admin");
 
 module.exports = async (req, res, next) => {
-  try {
-    const noneEmptyArray = await Admin.find();
-    if (noneEmptyArray.length > 0) {
-      next();
+    try {
+        const isFirst = await Admin.find();
+        if (isFirst.length > 0) {
+            next();
+        }
+
+        await Admin.create({
+            username: process.env.ADMIN_USERNAME,
+            email: process.env.ADMIN_EMAIL,
+            password: process.env.ADMIN_PASSWORD,
+        })
+
+        // const admin = new Admin({
+        //   username: process.env.ADMIN_USERNAME,
+        //   email: process.env.ADMIN_EMAIL,
+        //   password: process.env.ADMIN_PASSWORD,
+        // });
+        //
+        // await admin.save();
+        next();
+    } catch (e) {
+        console.log(e);
     }
-
-    const admin = new Admin({
-      username: process.env.ADMIN_USERNAME,
-      email: process.env.ADMIN_EMAIL,
-      password: process.env.ADMIN_PASSWORD,
-    });
-
-    await admin.save();
-    next();
-  } catch (e) {
-    console.log(e);
-  }
 };
