a7d7dd4b3e7d1be9819aa7b7527e4e7db6bda6cb
Daniel Marchenko <marchenkodanil97@gmail.com>
2022-02-11 13:39:17 +0200

    Add validation for get and delete requests

--- backend/routes/AdminRouter.js
+++ backend/routes/AdminRouter.js
@@ -1,26 +1,49 @@
 const express = require("express");
 const AdminController = require("../controllers/AdminController");
 const authMiddleware = require("../middlewares/authMiddleware");
-const {check} = require('express-validator')
-const checkIdMiddleware = require('../middlewares/validatingMongoIdMiddleware')
+const {check, param} = require('express-validator')
+const validatingMongoIdMiddleware = require('../middlewares/validatingMongoIdMiddleware')
 
 const adminRouter = new express.Router();
 
-adminRouter.post("/admin/create", [
-    check('username', 'Username must be filled').notEmpty(),
-    check('email', 'Email must be filled').isEmail().notEmpty(),
-    check('password', 'Password must be 4 to 8 characters').notEmpty().isLength({min: 4, max: 8}),
-], authMiddleware, AdminController.create);
-adminRouter.put("/admin/edit/:id", [
+adminRouter.post("/admin/create",
+    authMiddleware,
+    [
+        check('username', 'Username must be filled').notEmpty(),
+        check('email', 'Email must be filled').isEmail().notEmpty(),
+        check('password', 'Password must be 4 to 8 characters').notEmpty().isLength({min: 4, max: 8}),
+    ],
+    AdminController.create);
+
+adminRouter.put("/admin/edit/:id",
+    authMiddleware,
+    validatingMongoIdMiddleware,
     // param('id', 'Empty ID! Please enter ID!').notEmpty(), // @todo ??? should this work???
-    check('username', 'Username must be filled').notEmpty(),
-    check('email', 'Email must be filled').isEmail().notEmpty(),
-    check('password', 'Password must be 4 to 8 characters').notEmpty().isLength({min: 4, max: 8}),
-], authMiddleware, checkIdMiddleware, AdminController.update);
-adminRouter.delete("/admin/delete/:id", authMiddleware, AdminController.delete);
-adminRouter.get("/admin", authMiddleware, AdminController.getAll);
-adminRouter.get("/admin/user", authMiddleware, AdminController.getUser);
-adminRouter.get("/admin/:id", authMiddleware, AdminController.getOne);
-adminRouter.post("/admin/login", AdminController.login);
+    [
+        check('username', 'Username must be filled').notEmpty(),
+        check('email', 'Email must be filled').isEmail().notEmpty(),
+        check('password', 'Password must be 4 to 8 characters').notEmpty().isLength({min: 4, max: 8}),
+    ],
+    AdminController.update);
+adminRouter.delete("/admin/delete/:id",
+    authMiddleware,
+    validatingMongoIdMiddleware,
+    AdminController.delete);
+
+adminRouter.get("/admin",
+    authMiddleware,
+    AdminController.getAll);
+
+adminRouter.get("/admin/user",
+    authMiddleware,
+    AdminController.getUser);
+
+adminRouter.get("/admin/:id",
+    authMiddleware,
+    validatingMongoIdMiddleware,
+    AdminController.getOne);
+
+adminRouter.post("/admin/login",
+    AdminController.login);
 
 module.exports = adminRouter;
