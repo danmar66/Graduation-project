3c028140f80fcbf534afc566c2d40abfe15da129
Daniel Marchenko <marchenkodanil97@gmail.com>
2022-02-14 12:19:03 +0200

    Add "create" function to Order controller

--- backend/controllers/OrderController.js
+++ backend/controllers/OrderController.js
@@ -1,44 +1,65 @@
 const Order = require("../models/Order");
+const Product = require("../models/Product");
 
 class OrderController {
-  async create(req, res) {
-    try {
-      const {deliveryAddress, customerName, customerPhone, customerEmail} = req.body
-      const order = await Order.create({deliveryAddress, customerName, customerPhone, customerEmail})
-      res.json(order)
-    } catch (e) {
-      console.log(e.message); res.status(424).json({error: 'Unknown error'});
+    async create(req, res) {
+        try {
+            let {productItems, deliveryAddress, customerName, customerPhone, customerEmail} = req.body
+            let totalPrice = 0;
+            for (let i = 0; i < productItems.length; i++) {
+                const product = await Product.findById(productItems[i].item)
+                totalPrice = totalPrice + (product.price * productItems[i].quantity)
+            }
+            const order = await Order.create({
+                productItems,
+                deliveryAddress,
+                customerName,
+                customerPhone,
+                customerEmail,
+                totalPrice
+            })
+            res.json(order)
+        } catch (e) {
+            console.log(e.message);
+            res.status(424).json({error: 'Unknown error'});
+        }
     }
-  }
 
-  async getAll(req, res) {
-    try {
+    async getAll(req, res) {
+        try {
 
-    } catch (e) {
-      console.log(e.message); res.status(424).json({error: 'Unknown error'});
+        } catch (e) {
+            console.log(e.message);
+            res.status(424).json({error: 'Unknown error'});
+        }
     }
-  }
-  async getOne(req, res) {
-    try {
 
-    } catch (e) {
-      console.log(e.message); res.status(424).json({error: 'Unknown error'});
-    }
-  }
-  async update(req, res) {
-    try {
+    async getOne(req, res) {
+        try {
 
-    } catch (e) {
-      console.log(e.message); res.status(424).json({error: 'Unknown error'});
+        } catch (e) {
+            console.log(e.message);
+            res.status(424).json({error: 'Unknown error'});
+        }
     }
-  }
-  async delete(req, res) {
-    try {
-      
-    } catch (e) {
-      console.log(e.message); res.status(424).json({error: 'Unknown error'});
+
+    async update(req, res) {
+        try {
+
+        } catch (e) {
+            console.log(e.message);
+            res.status(424).json({error: 'Unknown error'});
+        }
     }
-  }
+
+    async delete(req, res) {
+        try {
+
+        } catch (e) {
+            console.log(e.message);
+            res.status(424).json({error: 'Unknown error'});
+        }
+    }
 }
 
 module.exports = new OrderController();
