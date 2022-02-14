7739c94c3631e404eaf34cae8cce6d572a8fb92b
Daniel Marchenko <marchenkodanil97@gmail.com>
2022-02-14 18:30:06 +0200

    edit type list

--- frontend/src/components/TypeList.tsx
+++ frontend/src/components/TypeList.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {Accordion} from "react-bootstrap";
 import {useActions} from "../hooks/useActions";
 import {useTypedSelector} from "../hooks/useTypedSelector";
@@ -7,6 +7,8 @@ import TagList from "./TagList";
 const TypeList: React.FC = () => {
     const {types, error, loading} = useTypedSelector((state) => state.type);
     const {tags} = useTypedSelector((state) => state.tag);
+
+    const [filterTags, setFilterTags] = useState<string[]>([])
 
     const {fetchTags} = useActions();
     useEffect(() => {
@@ -19,7 +21,25 @@ const TypeList: React.FC = () => {
     }, []);
 
     const handleFilters = ((filters: [string]) => {
-        console.log(filters);
+        // console.log(filters)
+
+        let temp = [...filterTags]
+
+        filters.map(tag => {
+            if (!temp.includes(tag)) {
+                temp.push(tag)
+            }
+        })
+
+        // temp.map(el => {
+        //     if (temp.includes(el)) {
+        //         return console.log('da')
+        //     } else {
+        //         return console.log('net')
+        //     }
+        // })
+        setFilterTags(temp)
+        console.log(filterTags)
     })
 
     if (loading) {
@@ -37,8 +57,11 @@ const TypeList: React.FC = () => {
                             {type.title.charAt(0).toUpperCase() + type.title.slice(1)}
                         </Accordion.Header>
                         <Accordion.Body>
-                            <TagList handleFilters={(filters: [string]) => handleFilters(filters)} typeId={type._id}
-                                     tagsList={tags}/>
+                            <TagList
+                                handleFilters={(filters: [string]) => handleFilters(filters)}
+                                typeId={type._id}
+                                tagsList={tags}
+                            />
                         </Accordion.Body>
                     </Accordion.Item>
                 ))}
