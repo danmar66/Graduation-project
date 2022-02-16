2b6f8782129342a0cddd7434cc5b2e3a6e03e2d8
Daniel Marchenko <marchenkodanil97@gmail.com>
2022-02-16 16:07:01 +0200

    Reformat code

--- frontend/src/router/AppRouter.tsx
+++ frontend/src/router/AppRouter.tsx
@@ -1,21 +1,20 @@
 import React from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
-import Shop from "../pages/Shop";
-import { authRoutes, publicRoutes } from "../routes";
+import {Navigate, Route, Routes} from "react-router-dom";
+import {authRoutes, publicRoutes} from "./routes";
 
 function AppRouter() {
-  const isAuth = true;
+    const isAuth = true;
 
-  return (
-    <Routes>
-      {/*<Route path="*" element={<Navigate replace to="/" />} />*/}
-      {isAuth &&
-        authRoutes.map(({ path, element }) => <Route key={path} path={path} element={element} />)}
-      {publicRoutes.map(({ path, element }) => (
-        <Route key={path} path={path} element={element} />
-      ))}
-    </Routes>
-  );
+    return (
+        <Routes>
+            {/*<Route path="*" element={<Navigate replace to="/" />} />*/}
+            {isAuth &&
+                authRoutes.map(({path, element}) => <Route key={path} path={path} element={element}/>)}
+            {publicRoutes.map(({path, element}) => (
+                <Route key={path} path={path} element={element}/>
+            ))}
+        </Routes>
+    );
 }
 
 export default AppRouter;
