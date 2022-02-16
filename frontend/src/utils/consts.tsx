bd19f18fd324f709cf3e888ab39be7178baf0a5e
Daniel Marchenko <marchenkodanil97@gmail.com>
2022-02-16 16:07:46 +0200

    Add const for Catalog

--- frontend/src/utils/consts.tsx
+++ frontend/src/utils/consts.tsx
@@ -3,4 +3,6 @@ export const PRODUCT_ROUTE = '/product'
 export const BASKET_ROUTE = '/basket'
 export const ADMIN_ROUTE = '/admin'
 export const LOGIN_ROUTE = '/login'
-export const SHOP_ROUTE = '/'
+export const HOME_PAGE = '/'
+export const CATALOG_ROUTE = "/catalog"
+
