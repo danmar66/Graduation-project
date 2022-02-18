3d88311af1c71c3fb020c46e9166395b77ff6f15
Daniel Marchenko <marchenkodanil97@gmail.com>
2022-02-18 11:06:26 +0200

    Intermediate result

--- backend/controllers/PaymentController.js
+++ backend/controllers/PaymentController.js
@@ -2,24 +2,14 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const Order = require("../models/Order");
 const Product = require("../models/Product");
 
-
 class PaymentController {
     async createSession(req, res) {
         try {
+            let preparedData = []
             const order = await Order.findById(req.body.id)
             const orderItems = order['productItems']
-            // console.log('order items ', orderItems)
-
-            // console.log(order['productItems'])
-
-            let result = []
-
             for (let i = 0; i < orderItems.length; i++) {
-                // console.log(orderItems[i])
                 const product = await Product.find({_id: orderItems[i]['item']})
-
-                // const products = await Product.find({_id: orderItems.map(product => product.item)})
-                // console.log('products ', products)
                 const productPrepare = product.map(item => {
                     return {
                         id: item._id,
@@ -28,50 +18,51 @@ class PaymentController {
                         qty: orderItems[i]['quantity']
                     }
                 })
-                console.log("products Prepared data = ", productPrepare)
-                result.push(productPrepare)
-
-
+                preparedData.push(productPrepare)
             }
+            // console.log(preparedData[0][0].title)
+            // console.log(preparedData[0])
 
-            console.log('RESULTTTT ', result)
 
-            // const products = await Product.find({_id: orderItems.map(product => product.item)})
-            // console.log('products ', products)
-            // const productPrepare = products.map(item => {
-            //     return {
-            //         id: item._id,
-            //         title: item.title,
-            //         price: item.price,
-            //     }
-            // })
-            // console.log("products Prepared data = ", productPrepare)
+            let line_items_data = preparedData.map(item => {
+                return {
+                    price_data: {
+                        currency: 'usd',
+                        product_data: {
+                            name: item[0].title,
+                        },
+                        unit_amount: item[0].price * 100,
+                    },
+                    quantity: item[0].qty
+                }
+            })
+            console.log(line_items_data)
 
-            // console.log('LOGG = ', orderItems[0]["item"] === productPrepare[0]['id'])
 
-            // const session = await stripe.checkout.sessions.create({
-            //     payment_method_types: ['card'],
-            //     mode: 'payment',
-            //     line_items: req.body.items.map(item => {
-            //         const storeItem = storeItems.get(item.id)
-            //         return {
-            //             price_data: {
-            //                 currency: 'usd',
-            //                 product_data: {
-            //                     name: storeItem.name
-            //                 },
-            //                 unit_amount: storeItem.priceInCents,
-            //             },
-            //             quantity: item.quantity
-            //         }
-            //     }),
-            //     success_url: `${process.env.SITE_URL}/catalog`,
-            //     cancel_url: `${process.env.SITE_URL}`
-            // })
+            const session = await stripe.checkout.sessions.create({
+                payment_method_types: ['card'],
+                mode: 'payment',
+                line_items: line_items_data,
 
-            // res.json({url: session.url})
-            res.json({message: 'OK'})
 
+                //     req.body.items.map(item => {
+                //     const storeItem = storeItems.get(item.id)
+                //     return {
+                //         price_data: {
+                //             currency: 'usd',
+                //             product_data: {
+                //                 name: storeItem.name
+                //             },
+                //             unit_amount: storeItem.priceInCents,
+                //         },
+                //         quantity: item.quantity
+                //     }
+                // }),
+                success_url: `${process.env.SITE_URL}/catalog`,
+                cancel_url: `${process.env.SITE_URL}`
+            })
+            res.json({url: session.url})
+            // res.json({message: 'OK'})
             // res.redirect(303, session.url)
         } catch (e) {
             console.log(e.message);
