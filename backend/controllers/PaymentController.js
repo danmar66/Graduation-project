d85e1dd5e6b56f12fe039409db1e4ea06a4af6f0
Daniel Marchenko <marchenkodanil97@gmail.com>
2022-02-18 11:09:23 +0200

    Made a payment function

--- backend/controllers/PaymentController.js
+++ backend/controllers/PaymentController.js
@@ -20,9 +20,6 @@ class PaymentController {
                 })
                 preparedData.push(productPrepare)
             }
-            // console.log(preparedData[0][0].title)
-            // console.log(preparedData[0])
-
 
             let line_items_data = preparedData.map(item => {
                 return {
@@ -36,33 +33,15 @@ class PaymentController {
                     quantity: item[0].qty
                 }
             })
-            console.log(line_items_data)
-
 
             const session = await stripe.checkout.sessions.create({
                 payment_method_types: ['card'],
                 mode: 'payment',
                 line_items: line_items_data,
-
-
-                //     req.body.items.map(item => {
-                //     const storeItem = storeItems.get(item.id)
-                //     return {
-                //         price_data: {
-                //             currency: 'usd',
-                //             product_data: {
-                //                 name: storeItem.name
-                //             },
-                //             unit_amount: storeItem.priceInCents,
-                //         },
-                //         quantity: item.quantity
-                //     }
-                // }),
                 success_url: `${process.env.SITE_URL}/catalog`,
                 cancel_url: `${process.env.SITE_URL}`
             })
             res.json({url: session.url})
-            // res.json({message: 'OK'})
             // res.redirect(303, session.url)
         } catch (e) {
             console.log(e.message);
