5c7709deeca7a172c90126f31bac19cf49a896a5
Daniel Marchenko <marchenkodanil97@gmail.com>
2022-02-23 12:32:13 +0200

    Refactoring router

--- frontend/src/router/AppRouter.tsx
+++ frontend/src/router/AppRouter.tsx
@@ -1,19 +1,37 @@
 import React from "react";
-import {Navigate, Route, Routes} from "react-router-dom";
-import {authRoutes, publicRoutes} from "./routes";
+import {Route, Routes} from "react-router-dom";
+import {PublicLayout} from "./layouts/PublicLayout";
+import {AdminLayout} from "./layouts/AdminLayout";
+import HomePage from "../pages/HomePage";
+import Catalog from "../pages/Catalog";
+import ProductPage from "../pages/ProductPage";
+import Basket from "../pages/Basket";
+
+import Admin from "../pages/Admin"
+import Test from "../pages/Test";
+import TagsTable from "../components/AdminPanelTables/TagsTable";
+import Auth from "../pages/Auth";
 
 function AppRouter() {
     const isAuth = true;
 
     return (
-        <Routes>
-            {/*<Route path="*" element={<Navigate replace to="/" />} />*/}
-            {isAuth &&
-                authRoutes.map(({path, element}) => <Route key={path} path={path} element={element}/>)}
-            {publicRoutes.map(({path, element}) => (
-                <Route key={path} path={path} element={element}/>
-            ))}
-        </Routes>
+        <>
+            <Routes>
+                <Route path='/' element={<PublicLayout/>}>
+                    <Route index element={<HomePage/>}/>
+                    <Route path='catalog' element={<Catalog/>}/>
+                    <Route path='product' element={<ProductPage/>}/>
+                    <Route path='basket' element={<Basket/>}/>
+                </Route>
+                <Route path='/admin' element={<AdminLayout/>}>
+                    <Route index element={<Admin/>}/>
+                    <Route path='test' element={<Test/>}/>
+                    <Route path='tags' element={<TagsTable/>}/>
+                </Route>
+                <Route path='/auth' element={<Auth/>}/>
+            </Routes>
+        </>
     );
 }
 
---
+++ frontend/src/router/layouts/AdminLayout.tsx
@@ -0,0 +1,13 @@
+import {Outlet} from "react-router-dom"
+import AdminNavBar from "../../components/AdminNavBar";
+
+const AdminLayout = () => {
+    return (
+        <>
+            <AdminNavBar/>
+            <Outlet/>
+        </>
+    )
+}
+
+export {AdminLayout}
