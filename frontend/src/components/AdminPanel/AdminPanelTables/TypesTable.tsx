5691ec719d5145f9f35f2012240b979a4ad07153
Daniel Marchenko <marchenkodanil97@gmail.com>
2022-03-18 19:42:38 +0200

    Add deleteType action

--- frontend/src/components/AdminPanel/AdminPanelTables/TypesTable.tsx
+++ frontend/src/components/AdminPanel/AdminPanelTables/TypesTable.tsx
@@ -5,29 +5,39 @@ import {useTypedSelector} from "../../../hooks/useTypedSelector";
 import TableTemplate from "./TableTemplate";
 import {useNavigate} from "react-router-dom";
 import {ADMIN_ROUTE} from "../../../utils/consts";
+import axios from "axios";
 
 const TypesTable = () => {
     const {types, loading, error} = useTypedSelector(state => state.type)
 
     const {fetchTypes} = useActions()
+    const {deleteType} = useActions()
 
     useEffect(() => {
         fetchTypes(1);
+        console.log(types)
     }, []);
 
     const navigate = useNavigate();
 
     const handleEdit = (id: string) => {
         navigate(ADMIN_ROUTE + `/types/edit/${id}`)
-        console.log('handle edit id ', id)
+        // console.log('handle edit id ', id)
     }
     const handleDelete = (id: string) => {
         console.log('handle delete id ', id)
+        // axios.delete(`http://localhost:5000/api/tag_type/delete/${id}`)
+        //     .then(res => {
+        //         console.log(res);
+        //         // alert('Type deleted!')
+        //     })
+        //     .catch(err => console.error(err))
+        deleteType(id)
 
     }
 
     const handlePage = (page: number) => {
-        console.log('handle page ' , page)
+        console.log('handle page ', page)
         fetchTypes(page)
     }
 
