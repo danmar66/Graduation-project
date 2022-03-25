a095d3aea84db743125a2d56312488f8b011c2d4
Daniel Marchenko <marchenkodanil97@gmail.com>
2022-03-25 11:12:07 +0200

    Add validation, auth header to axios request

--- frontend/src/components/AdminPanel/createPages/CreateTypePage.tsx
+++ frontend/src/components/AdminPanel/createPages/CreateTypePage.tsx
@@ -1,39 +1,41 @@
 import React, {useState} from 'react';
-import {Button, FloatingLabel, Form} from "react-bootstrap";
+import {Button, Form} from "react-bootstrap";
 import axios from "axios";
 
 const CreateTypePage = () => {
     const [title, setTitle] = useState('')
     const [slug, setSlug] = useState('')
-
+    const token = window.localStorage.getItem('authToken') || ''
 
     return (<div className='d-flex justify-content-center'>
         <Form
             className='col-md-4'
             onSubmit={event => {
                 event.preventDefault();
-                let data = JSON.stringify({title, slug})
-                console.log(data)
-                axios.post('http://localhost:5000/api/tag_type/create', {title, slug})
-                    .then(res => console.log(res))
+                axios.post('http://localhost:5000/api/tag_type/create',
+                    {title, slug},
+                    {
+                        headers: {
+                            'Authorization': token
+                        }
+                    })
+                    .then(res => {
+                        setSlug('')
+                        setTitle('')
+                        alert(`Type "${title}" with slug "${slug}" created!`)
+                    })
                     .catch(err => console.error(err))
             }}
         >
             <Form.Group className='mb-3'>
-                {/*<Form.Label>Title</Form.Label>*/}
-                <FloatingLabel
-                    controlId='floatingInput'
-                    label='title'
-                >
-                    <Form.Control value={title} type='title' placeholder={'Title'} onChange={(event => {
-                        setTitle(event.target.value)
-                    })}/>
-                </FloatingLabel>
-
+                <Form.Label>Title</Form.Label>
+                <Form.Control value={title} type='title' placeholder={'Enter title...'} onChange={(event => {
+                    setTitle(event.target.value)
+                })}/>
             </Form.Group>
             <Form.Group className='mb-3'>
                 <Form.Label>Slug</Form.Label>
-                <Form.Control value={slug} type='slug' placeholder={'Slug'} onChange={(event => {
+                <Form.Control value={slug} type='slug' placeholder={'Enter slug...'} onChange={(event => {
                     setSlug(event.target.value)
                 })}/>
             </Form.Group>
